af6fb0edbfaf4d7e456c63ef70a3eeb7ee921ebc
NiraiHayakawa <91765262+NiraiHayakawa@users.noreply.github.com>
2022-12-04 03:12:55 -0800

    Initial commit

---
+++ NOKIN_APPS_APPCODE/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  NOKIN_APPS_APPCODE
+//
+//  Created by geo on 2022/12/04.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NOKIN_APPS_APPCODE/NOKIN_APPS_APPCODEApp.swift
@@ -0,0 +1,18 @@
+//
+//  NOKIN_APPS_APPCODEApp.swift
+//  NOKIN_APPS_APPCODE
+//
+//  Created by geo on 2022/12/04.
+//
+//
+
+import SwiftUI
+
+@main
+struct NOKIN_APPS_APPCODEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
