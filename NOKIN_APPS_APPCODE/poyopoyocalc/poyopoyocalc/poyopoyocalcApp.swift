6fd2b6009c2e1f99a3b16657dc851d5f7a59facf
NiraiHayakawa <91765262+NiraiHayakawa@users.noreply.github.com>
2022-12-04 03:34:29 -0800

    iroiro

---
+++ NOKIN_APPS_APPCODE/poyopoyocalc/poyopoyocalc/poyopoyocalcApp.swift
@@ -0,0 +1,18 @@
+//
+//  poyopoyocalcApp.swift
+//  poyopoyocalc
+//
+//  Created by geo on 2022/12/04.
+//
+//
+
+import SwiftUI
+
+@main
+struct poyopoyocalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
